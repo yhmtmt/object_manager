f148aa077b0908ef24141ad2d543c93fe86deb38
Yohei Matsumoto <yhmtmt@gmail.com>
2019-12-19 16:35:50 +0900

    Update for build system on awsv1

--- f_object_manager.cpp
+++ f_object_manager.cpp
@@ -1,89 +1,65 @@
 // Copyright(c) 2016 Yohei Matsumoto, All right reserved. 
 
-// f_obj_manager.cpp is free software: you can redistribute it and/or modify
+// f_object_manager.cpp is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// f_obj_manager.cpp is distributed in the hope that it will be useful,
+// f_object_manager.cpp is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with f_obj_manager.cpp.  If not, see <http://www.gnu.org/licenses/>. 
-
-#include <cstdio>
-#include <cstring>
-#include <cmath>
-
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <list>
-#include <map>
-
-using namespace std;
-
-#include "../util/aws_stdlib.h"
-#include "../util/aws_thread.h"
-#include "../util/c_clock.h"
-
-#include <opencv2/opencv.hpp>
-
-using namespace cv;
+// along with f_object_manager.cpp.  If not, see <http://www.gnu.org/licenses/>. 
 
 
-#include "f_obj_manager.h"
+#include "f_object_manager.hpp"
+DEFINE_FILTER(f_object_manager)
 
-f_obj_manager::f_obj_manager(const char * name): f_base(name), m_state(NULL), m_ais_obj(NULL),
-	m_range(10000), m_dtold(180 * SEC)
+
+f_object_manager::f_object_manager(const char * name): f_base(name), m_state(NULL), m_ais_obj(NULL), m_range(10000), m_dtold(180 * SEC)
 {
-	register_fpar("ch_state", (ch_base**)&m_state, typeid(ch_state).name(), "State channel");
-	register_fpar("ch_ais_obj", (ch_base**)&m_ais_obj, typeid(ch_ais_obj).name(), "AIS object channel.");
-	register_fpar("ch_obj", (ch_base**)&m_obj, typeid(ch_obj).name(), "Generic object channel.");
-	register_fpar("dtold", &m_dtold, "Time the objects alive from their update.");
-	register_fpar("range", &m_range, "The object range of interest.");
+  register_fpar("ch_state", (ch_base**)&m_state, typeid(ch_state).name(), "State channel");
+  register_fpar("ch_ais_obj", (ch_base**)&m_ais_obj, typeid(ch_ais_obj).name(), "AIS object channel.");
+  register_fpar("dtold", &m_dtold, "Time the objects alive from their update.");
+  register_fpar("range", &m_range, "The object range of interest.");
 }
 
-f_obj_manager::~f_obj_manager()
+f_object_manager::~f_object_manager()
 {
 }
 
-bool f_obj_manager::init_run()
+bool f_object_manager::init_run()
 {
-	if (!m_state)
-		return false;
-	return true;
+  if (!m_state)
+    return false;
+  return true;
 }
 
-void f_obj_manager::destroy_run()
+void f_object_manager::destroy_run()
 {
 }
 
-bool f_obj_manager::proc()
+bool f_object_manager::proc()
 {
-  Mat Renu;
+  double * Renu;
   double x, y, z;
   float r, p;
   float vox, voy;
   long long t = 0;
-  Renu = m_state->get_enu_rotation(t);
+  m_state->get_enu_rotation(t, Renu);
   m_state->get_position_ecef(t, x, y, z);
   m_state->get_velocity_vector(t, vox, voy);
   
   if(m_ais_obj){
     // update enu coordinate
-    if(!Renu.empty()){
-      m_ais_obj->update_rel_pos_and_vel(Renu, x, y, z);
-      m_ais_obj->remove_old(get_time() - m_dtold);
-      m_ais_obj->remove_out(m_range);
-    }
+    m_ais_obj->update_rel_pos_and_vel(Renu, x, y, z);
+    m_ais_obj->remove_old(get_time() - m_dtold);
+    m_ais_obj->remove_out(m_range);
+    
     m_ais_obj->reset_updates();
     m_ais_obj->calc_tdcpa(t, vox, voy);
-  }
-  
-  if(m_obj){
   }
   
   return true;
--- f_object_manager.hpp
+++ f_object_manager.hpp
@@ -1,45 +1,44 @@
 // Copyright(c) 2016 Yohei Matsumoto, All right reserved. 
 
-// f_obj_manager.h is free software: you can redistribute it and/or modify
+// f_object_manager.hpp is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// f_obj_manager.h is distributed in the hope that it will be useful,
+// f_object_manager.hpp is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with f_obj_manager.h.  If not, see <http://www.gnu.org/licenses/>. 
+// along with f_object_manager.hpp.  If not, see <http://www.gnu.org/licenses/>. 
 
-#ifndef _F_OBJ_MANAGER_H_
-#define _F_OBJ_MANAGER_H_
+#ifndef F_OBJECT_MANAGER_HPP
+#define F_OBJECT_MANAGER_HPP
 
-#include "f_base.h"
-#include "../channel/ch_state.h"
-#include "../channel/ch_obj.h"
+#include "filter_base.hpp"
+#include "ch_state.hpp"
+#include "ch_obj.hpp"
 
 // Description:
 // The filter manages the objects, and shares objects between hosts efficiently.
 // 
-class f_obj_manager: public f_base
+class f_object_manager: public f_base
 {
 protected:
-	ch_state * m_state;
-	ch_ais_obj * m_ais_obj;
-	ch_obj * m_obj;
+  ch_state * m_state;
+  ch_ais_obj * m_ais_obj;
 
-	long long m_dtold;
-	float m_range;
+  long long m_dtold;
+  float m_range;
 
 public:
-	f_obj_manager(const char * name);
-	virtual ~f_obj_manager();
+  f_object_manager(const char * name);
+  virtual ~f_object_manager();
 
-	virtual bool init_run();
-	virtual void destroy_run();
-	virtual bool proc();
+  virtual bool init_run();
+  virtual void destroy_run();
+  virtual bool proc();
 };
 
 #endif
