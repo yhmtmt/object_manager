9f533252407da9d048a82ad8e329118d5386bd9c
Yohei Matsumoto <yhmtmt@gmail.com>
2020-04-19 11:44:20 +0900

    Minor update

--- f_object_manager.hpp
+++ f_object_manager.hpp
@@ -1,4 +1,4 @@
-// Copyright(c) 2016 Yohei Matsumoto, All right reserved. 
+// Copyright(c) 2016-2020 Yohei Matsumoto, All right reserved. 
 
 // f_object_manager.hpp is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
